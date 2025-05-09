2d0fd18f75f0d47c240a43fd25f5533fddd31d5c
Andrey Klimentev <andrei_klimentev@mail.ru>
2025-05-09 23:03:48 +0300

    add classes and tests

---
+++ tests/test_products_classes.py
@@ -0,0 +1,18 @@
+def test_product_class(first_product,second_product):
+    assert first_product.name == "Samsung Galaxy S23 Ultra"
+    assert first_product.description == "256GB, Серый цвет, 200MP камера"
+    assert first_product.price == 180000.0
+    assert first_product.quantity== 5
+    assert second_product.name == "Iphone 15"
+    assert second_product.description == "512GB, Gray space"
+    assert second_product.price == 210000.0
+    assert second_product.quantity== 8
+
+
+def test_category_lass(first_category):
+    assert first_category.name == "Смартфоны"
+    assert first_category.description == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+    assert first_category.name == "Смартфоны"
+    assert len(first_category.products) == 2
+    assert first_category.category_count == 1
+    assert first_category.product_count == 2
