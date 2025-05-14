607266c5f2d8458ddb78995158164553836d5e7b
Andrey Klimentev <andrei_klimentev@mail.ru>
2025-05-15 01:00:23 +0300

    add new functional in classes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,39 +1,37 @@
 import pytest
-from src.products_classes import Product,Category
+
+from src.products_classes import Category, Product
+
 
 @pytest.fixture
 def first_product():
-    return Product(
-        "Samsung Galaxy S23 Ultra",
-        "256GB, Серый цвет, 200MP камера",
-        180000.0,
-        5
-    )
+    return Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
 
 
 @pytest.fixture
 def second_product():
-    return Product(
-        "Iphone 15",
-        "512GB, Gray space",
-        210000.0,
-        8
-    )
+    return Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+
+
+@pytest.fixture
+def product_dict():
+    product_dict = {
+        "name": "HUAWEY",
+        "description": "256GB," " Серый цвет, 200MP камера",
+        "price": 25600,
+        "quantity": 22,
+    }
+    return product_dict
 
 
 @pytest.fixture
 def first_category():
-    category1 = Category("Смартфоны",
-                     "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-                     [Product(
-        "Samsung Galaxy 1337 Mega Super Puper Ultra",
-        "256GB, Серый цвет, 200MP камера",
-        180000.0,
-        5
-    ), Product(
-        "Iphone 9999",
-        "512GB, Gray space",
-        210000.0,
-        8
-    )])
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [
+            Product("Samsung Galaxy 1337 Mega Super Puper Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5),
+            Product("Iphone 9999", "512GB, Gray space", 210000.0, 8),
+        ],
+    )
     return category1
